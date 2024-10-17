315ab34dbf7cc63a10ac3c78412ff2c898954dc5
michaelbohmann <bohmanmh@mail.uc.edu>
2024-10-17 14:43:17 -0400

    Initial commit

---
+++ src/SafeInput.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class SafeInput
+{
+
+    /**
+     * get a String Val from the user which must be at least one character
+     *
+     * @param pipe scanner to use to read the input
+     * @param prompt prompt to tell the user what to input
+     * @return String that is at least one character
+     */
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
+
+        do
+        {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+            if(retVal.isEmpty())
+            {
+                System.out.println("You must enter at least on character!");
+            }
+
+        }while(retVal.isEmpty());
+
+        return retVal;
+    }
+
+    /**
+     * gets an unconstrained int value from the user
+     * @param pipe Scanner to use for input
+     * @param prompt prompt that tells the user what to input
+     * @return an unconstrained int value
+     */
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retVal = 0;
+        String trash = " ";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            if (pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a valid integer not" + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+
+
+
+}
+
+
